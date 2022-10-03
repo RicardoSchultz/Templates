7e3e460ced0c085f78093534f872df8cdf63e44c
RicardoSchultz <schultz.ricardo@hotmail.com>
2022-10-02 22:57:42 -0300

    Ajustes Script

--- Template/assets/js/script.js
+++ Template/assets/js/script.js
@@ -1,17 +1,17 @@
-
-    //const modal = document.querySelector('.conteudo');
-    //css(modal, {
-    //'display': 'block'
-    //});
-   
-
-
     function content() {
-        var element = document.getElementById("conteudo").innerHTML = "Teste Conteudo";
-        element.innerHTML = '<div>teste div conteudo</div>'
+        var element = document.getElementById("arq");
+        element.scr = "C:/Projetos/Teste/Template/arquivos/ArquivoTeste.pdf";
+        //element.innerHTML = '<iframe width="100%" height="400" src="https://dspbiomedical.com/wp-content/uploads/2017/12/teste-de-tamanho-de-arquivo-1.pdf"></iframe>';
+        //element.innerHTML = '<iframe width="100%" height="400" src="C:/Projetos/Teste/Template/arquivos/ArquivoTeste.docx"/>';
+        //element.innerHTML = '<iframe width="100%" height="400" class="doc" src="https://docs.google.com/gview?url=http://writing.engr.psu.edu/workbooks/formal_report_template.doc&embedded=true"></iframe>';
+        //element.innerHTML = '<iframe src="https://docs.google.com/gview?url=C:/Projetos/Teste/Template/arquivos/ArquivoTeste.pdf&embedded=true"></iframe>';
+
+        //C:/Projetos/Teste/Template/arquivos/ArquivoTeste.pdf
 
 
-        return ;
+
+        console.log(element);
+        return element ;
       }
 
 
